bf421b4be65c52dbdeeffb1204fe3216a2d47b0c
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-01 22:59:17 +0530

    (feat): fav contacts styles

--- lib/widgets/favorite_contacts.dart
+++ lib/widgets/favorite_contacts.dart
@@ -5,14 +5,32 @@ class FavoriteContacts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Text('Favorite Contacts'),
-        IconButton(
-          icon: Icon(
-            Icons.more_horiz,
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Favorite Contacts',
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.0,
+                ),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.more_horiz,
+                ),
+                iconSize: 30.0,
+                color: Colors.blueGrey,
+                onPressed: () {},
+              ),
+            ],
           ),
-          onPressed: () {},
         ),
       ],
     );
