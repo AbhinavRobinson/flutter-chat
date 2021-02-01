728796944efef00af429c9a813e7260409930744
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-01 22:38:06 +0530

    (feat): setState onTap for selected category

--- lib/widgets/category_selector.dart
+++ lib/widgets/category_selector.dart
@@ -19,15 +19,23 @@ class _CategorySelectorState extends State<CategorySelector> {
         scrollDirection: Axis.horizontal,
         itemCount: categories.length,
         itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-            child: Text(
-              categories[index],
-              style: TextStyle(
-                  color: index == selectedIndex ? Colors.white : Colors.white60,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.2),
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+              child: Text(
+                categories[index],
+                style: TextStyle(
+                    color:
+                        index == selectedIndex ? Colors.white : Colors.white60,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2),
+              ),
             ),
           );
         },
